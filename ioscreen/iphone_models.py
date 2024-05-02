071e4c721f75ba23a94e1efd4ecf69e6bc542f67
Lorenzo Ferron <lorenzo_ferron@hotmail.it>
2024-05-02 13:14:32 +0200

    Add new iPhones

--- ioscreen/iphone_models.py
+++ ioscreen/iphone_models.py
@@ -29,22 +29,30 @@ class iPhoneModels:
         'iPhone14,8': {'model': 'iPhone 14 Plus', 'width': 547},
         'iPhone15,2': {'model': 'iPhone 14 Pro', 'width': 435},                         # 6,1"
         'iPhone15,3': {'model': 'iPhone 14 Pro Max', 'width': 547},
+        'iPhone15,4': {'model': 'iPhone 15', 'width': 435},                             # 6,1"
+        'iPhone15,5': {'model': 'iPhone 15 Plus', 'width': 547},
+        'iPhone16,1': {'model': 'iPhone 15 Pro', 'width': 435},                         # 6,1"
+        'iPhone16,2': {'model': 'iPhone 15 Pro Max', 'width': 547},
+        'iPhone14,1': {'model': 'Unknown iPhone', 'width': 435},                        # probably iPhone 13 (USB-C)
+                                                                                        # A15 Chip by aaronp613
+        'iPhone14,9': {'model': 'Unknown iPhone', 'width': 547},                        # probably iPhone 13 mini (USB-C)
+                                                                                        # A15 Chip by aaronp613
         'iPhone2,1': {'model': 'iPhone 3GS', 'width': 547},
         'iPhone3,1': {'model': 'iPhone 4 (GSM)', 'width': 547},
         'iPhone3,2': {'model': 'iPhone 4 (GSM, 2012)', 'width': 547},
         'iPhone3,3': {'model': 'iPhone 4 (CDMA)', 'width': 547},
         'iPhone4,1': {'model': 'iPhone 4S', 'width': 547},
-        'iPhone5,1': {'model': 'iPhone 5 (GSM)', 'width': 547},
-        'iPhone5,2': {'model': 'iPhone 5 (CDMA)', 'width': 547},
-        'iPhone5,3': {'model': 'iPhone 5c (GSM)', 'width': 547},
-        'iPhone5,4': {'model': 'iPhone 5c (CDMA)', 'width': 547},
-        'iPhone6,1': {'model': 'iPhone 5s (GSM)', 'width': 547},
-        'iPhone6,2': {'model': 'iPhone 5s (CDMA)', 'width': 547},
+        'iPhone5,1': {'model': 'iPhone 5 (GSM)', 'width': 525},                         # 4″
+        'iPhone5,2': {'model': 'iPhone 5 (CDMA)', 'width': 525},                        # 4″
+        'iPhone5,3': {'model': 'iPhone 5c (GSM)', 'width': 525},                        # 4″
+        'iPhone5,4': {'model': 'iPhone 5c (CDMA)', 'width': 525},                       # 4″
+        'iPhone6,1': {'model': 'iPhone 5s (GSM)', 'width': 525},                        # 4″
+        'iPhone6,2': {'model': 'iPhone 5s (CDMA)', 'width': 525},                       # 4″
         'iPhone7,1': {'model': 'iPhone 6 Plus', 'width': 529},                          # 5,5"
         'iPhone7,2': {'model': 'iPhone 6', 'width': 529},                               # 4,7"
         'iPhone8,1': {'model': 'iPhone 6s', 'width': 529},                              # 4,7"
         'iPhone8,2': {'model': 'iPhone 6s Plus', 'width': 529},                         # 5,5"
-        'iPhone8,4': {'model': 'iPhone SE (1st generation)', 'width': 547},
+        'iPhone8,4': {'model': 'iPhone SE (1st generation)', 'width': 525},             # 4″
         'iPhone9,1': {'model': 'iPhone 7 (CDMA)', 'width': 529},                        # 4,7"
         'iPhone9,2': {'model': 'iPhone 7 Plus (CDMA)', 'width': 529},                   # 5,5"
         'iPhone9,3': {'model': 'iPhone 7 (GSM)', 'width': 529},                         # 4,7"
